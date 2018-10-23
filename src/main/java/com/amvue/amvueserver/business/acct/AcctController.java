df7ae8b0488af97b4ab2fd8fdf3137e7fb0d8a27
martSforever <marts_forever@163.com>
2018-10-23 22:38:18 +0800

    server port adjust

--- src/main/java/com/amvue/amvueserver/business/acct/AcctController.java
+++ src/main/java/com/amvue/amvueserver/business/acct/AcctController.java
@@ -1,16 +1,11 @@
 package com.amvue.amvueserver.business.acct;
 
-import com.amvue.amvueserver.core.ControllerUtils;
 import com.amvue.amvueserver.core.basic.BasicController;
 import com.amvue.amvueserver.core.basic.BasicService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("acct")
